ef6f4a5637b03192f4bcafb0a8b5b34dc977e204
TGurl <geertjevandijk70@gmail.com>
2024-10-17 20:13:18 +0200

    init mkzip and munzip
    Initial commits

---
+++ mkzip/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class Mkzip:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = Mkzip()
+    app.main()
---
+++ munzip/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class MUnzip:
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = MUnzip()
+    app.main()
